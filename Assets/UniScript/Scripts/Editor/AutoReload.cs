7cd3b4855f833802ed5da0c00b40a8bdacca9b36
Park June Chul <pjc0247@naver.com>
2019-04-08 01:49:00 +0900

    Add files via upload

---
+++ Assets/UniScript/Scripts/Editor/AutoReload.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AutoReload : AssetPostprocessor
+{
+    private static Dictionary<string, List<UniFileScriptBehaviour>> scripts 
+        = new Dictionary<string, List<UniFileScriptBehaviour>>();
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Init()
+    {
+#if UNITY_EDITOR
+        var go = new GameObject("AutoloadIndicator");
+        go.AddComponent<AutoLoadIndicator>();
+
+        UniFileScriptBehaviour.registerScriptDelegate = (path, obj) => {
+            if (scripts.ContainsKey(path) == false)
+                scripts[path] = new List<UniFileScriptBehaviour>();
+            scripts[path].Add(obj);
+        };
+        UniFileScriptBehaviour.unregisterScriptDelegate = (path, obj) => {
+            if (scripts.ContainsKey(path) == false)
+                return;
+            scripts[path].Remove(obj);
+        };
+#endif
+    }
+
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        if (Application.isPlaying == false)
+            return;
+
+        var changes = new List<string>();
+        foreach (var asset in importedAssets)
+        {
+            if (asset.EndsWith(".csx") == false)
+                continue;
+            
+            if (scripts.ContainsKey(asset))
+            {
+                foreach (var obj in scripts[asset])
+                    obj.ReloadScript();
+
+                AutoLoadIndicator.applied = true;
+            }
+        }
+    }
+}
---
+++ Assets/UniScript/Scripts/Editor/UniFileScriptBehaviourEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(UniFileScriptBehaviour))]
+public class UniFileScriptBehaviourEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var t = (UniFileScriptBehaviour)target;
+        if (GUI.changed && t.script != null)
+            t.scriptPath = AssetDatabase.GetAssetPath(t.script);
+    }
+}
