05f46374ce8f72e493e4a357120974838a8e107c
Park June Chul <pjc0247@naver.com>
2019-04-28 15:14:19 +0900

    [WIP] WebPlayground demo

---
+++ Assets/UniScript/Demo/WebPlayground/EnableWebGLKeyInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Neccessary to handle keyboard in HTML other elements.
+/// https://docs.unity3d.com/Manual/webgl-input.html
+/// </summary>
+public class EnableWebGLKeyInput : MonoBehaviour
+{
+    void Start()
+    {
+#if UNITY_WEBGL
+        WebGLInput.captureAllKeyboardInput = false;
+#endif
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+            WebGLInput.captureAllKeyboardInput = true;
+        else
+            WebGLInput.captureAllKeyboardInput = false;
+    }
+}
