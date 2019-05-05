e79b45c757359438a226dd308dad29c52bbdc601
Park June Chul <pjc0247@naver.com>
2019-05-05 20:16:35 +0900

    Add comments

--- Assets/UniScript/Demo/AssetBundle/LoadUniScriptScene.cs
+++ Assets/UniScript/Demo/AssetBundle/LoadUniScriptScene.cs
@@ -6,6 +6,11 @@ public class LoadUniScriptScene : MonoBehaviour
 {
     void Start()
     {
+        // If you face 404 not found error, 
+        // you should build AssetBundles first.
+        //
+        // Click `Build/BuildAssetBundle` menu on Unity Editor.
+        // (or please take a look `Editor/CreateAssetBundle.cs`)
         UniAssetBundle.LoadUniScriptScene(
             "file:///Packs/testbundle",
             "file:///Packs/testbundle_script");
