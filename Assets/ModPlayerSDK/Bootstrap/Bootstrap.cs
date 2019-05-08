00665564e78b74fa2217982218074a72b3fad498
Park June Chul <pjc0247@naver.com>
2019-05-08 18:00:57 +0900

    update

--- Assets/ModPlayerSDK/Bootstrap/Bootstrap.cs
+++ Assets/ModPlayerSDK/Bootstrap/Bootstrap.cs
@@ -3,62 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Photon.Pun;
-
-using PhotonHashtable = ExitGames.Client.Photon.Hashtable;
 using Photon.Realtime;
+using PhotonHashtable = ExitGames.Client.Photon.Hashtable;
 
-public class Bootstrap : MonoBehaviourPunCallbacks
+namespace ModPlayerSDK.Internal
 {
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    private static void Initialize()
+    public class Bootstrap : MonoBehaviourPunCallbacks
     {
-        var gobj = new GameObject("ModPlayerBootstrap");
-        gobj.AddComponent<Bootstrap>();
-        gobj.AddComponent<PlayerSpawn>();
-        gobj.AddComponent<InputManager>();
-        DontDestroyOnLoad(gobj);
-    }
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize()
+        {
+            var gobj = new GameObject("ModPlayerBootstrap");
+            gobj.AddComponent<Bootstrap>();
+            gobj.AddComponent<PlayerSpawn>();
+            gobj.AddComponent<InputManager>();
+            DontDestroyOnLoad(gobj);
+        }
 
-    void Awake()
-    {
-        PhotonNetwork.ConnectUsingSettings();
-    }
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("[Connected]");
+        void Awake()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("[Connected]");
 
-        PhotonNetwork.LocalPlayer.SetCustomProperties(new PhotonHashtable() {
-            ["character"] = "voxelgirl_black"
-        });
-        PhotonNetwork.NickName = "EditorPlayer";
+            PhotonNetwork.LocalPlayer.SetCustomProperties(new PhotonHashtable()
+            {
+                ["character"] = "voxelgirl_black"
+            });
+            PhotonNetwork.NickName = "EditorPlayer";
 
-        CreateRoom("test", RoomType.Team, null);
-    }
+            CreateRoom("test", RoomType.Team, null);
+        }
 
-    public void CreateRoom(string name, string roomType, string password)
-    {
-        var opt = new RoomOptions();
-        opt.PublishUserId = true;
-        opt.IsVisible = true;
-        opt.MaxPlayers = 4;
-        opt.CustomRoomPropertiesForLobby = new string[] { "name", "type", "version", "password" };
-        opt.CustomRoomProperties = new PhotonHashtable() {
-            ["name"] = name,
-            ["type"] = roomType,
-            ["password"] = password,
-            ["version"] = Application.version,
-            ["isChatAvaliable"] = true,
-            ["isPvpAvaliable"] = true,
-            ["is3rdPersonCam"] = roomType == RoomType.Chat || roomType == RoomType.Cinema ? true : false
-        };
-        PhotonNetwork.CreateRoom(null, opt, TypedLobby.Default);
-    }
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("[OnJoinedRoom]");
-    }
-    public override void OnJoinRoomFailed(short returnCode, string message)
-    {
-        Debug.Log("[OnJoinRoomFailed] " + message);
+        public void CreateRoom(string name, string roomType, string password)
+        {
+            var opt = new RoomOptions();
+            opt.PublishUserId = true;
+            opt.IsVisible = true;
+            opt.MaxPlayers = 4;
+            opt.CustomRoomPropertiesForLobby = new string[] { "name", "type", "version", "password" };
+            opt.CustomRoomProperties = new PhotonHashtable()
+            {
+                ["name"] = name,
+                ["type"] = roomType,
+                ["password"] = password,
+                ["version"] = Application.version,
+                ["isChatAvaliable"] = true,
+                ["isPvpAvaliable"] = true,
+                ["is3rdPersonCam"] = roomType == RoomType.Chat || roomType == RoomType.Cinema ? true : false
+            };
+            PhotonNetwork.CreateRoom(null, opt, TypedLobby.Default);
+        }
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("[OnJoinedRoom]");
+        }
+        public override void OnJoinRoomFailed(short returnCode, string message)
+        {
+            Debug.Log("[OnJoinRoomFailed] " + message);
+        }
     }
 }
--- Assets/ModPlayerSDK/Server/Model/ModApp.cs
+++ Assets/ModPlayerSDK/Server/Model/ModApp.cs
@@ -10,9 +10,18 @@ namespace ModPlayerSDK.Model
         public string owner;
         public string name;
         public string thumbnail_url;
-
         public ModBuild[] builds;
 
+        // Runtime properties
         public Texture2D thumbnail;
+        public ModBuild last_build
+        {
+            get
+            {
+                if (builds == null || builds.Length == 0)
+                    return null;
+                return builds[builds.Length - 1];
+            }
+        }
     }
 }
